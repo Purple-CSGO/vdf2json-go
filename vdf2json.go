90fe71143798d092af4b8cd876e0692e6fce1f86
purp1e <438518244@qq.com>
2020-09-05 23:29:47 +0800

    0.0.2

--- vdf2json.go
+++ vdf2json.go
@@ -1,9 +1,10 @@
 package vdf2json_go
 
-func Vdf2json(vdfData string) string {
-	return vdfData
-}
+func ToJson(vdfData string) (string, error) {
+	// - 按行遍历 如果有 \"\t\t\" -> \": \" 同时在结尾加上,
+	// - 按行遍历 如果有 { → : { ... 如果有 } → },
+	// - trim每行左边的 \t 和每行右边的换行符 \r 和 \n
+	// - 遍历 如果有 },} → }}
 
-func ToJson(vdfData string) string {
-	return vdfData
+	return "", nil
 }
